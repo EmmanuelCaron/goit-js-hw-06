2c19955145aaca47d99af30be0541adb22d91575
Emmanuel <petru.mihai95@e-uvt.ro>
2024-04-09 01:17:20 +0300

    tema 1 update 1

--- javascript-homework-main/v2/06/src/js/task-01.js
+++ javascript-homework-main/v2/06/src/js/task-01.js
@@ -2,8 +2,8 @@ const categories = document.querySelectorAll('#categories .item');
 
 console.log(`Total categories: ${categories.length}`);
 
-categories.forEach (pula => {
-    const title = pula.querySelector('h2').textContent;
-    const items = pula.querySelectorAll('li').length;
+categories.forEach (categorii => {
+    const title = categorii.querySelector('h2').textContent;
+    const items = categorii.querySelectorAll('li').length;
     console.log(`Title: ${title}, Number of items: ${items}`);
 });
